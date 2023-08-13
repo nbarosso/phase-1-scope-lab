7344a7a58240e55c0e9468881c58df46a8622b30
Nicholas Barosso <nbarosso@gmail.com>
2023-08-13 09:44:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'jordan'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toLocaleUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'nick'
+}
